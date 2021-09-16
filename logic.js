85a95f2c3ee82fd1eea4237b1d1241c357cb9143
Minh Nguyen <mnx2012@gmail.com>
2021-09-16 18:06:16 +0700

    Add dynamic property

--- logic.js
+++ logic.js
@@ -3,7 +3,9 @@ const colors = ["#C6EBC9", "#70AF85", "#F0E2D0", "#AA8976"];
 const myUI = document.getElementById("my-ui");
 
 // binding state with listener;
-const myState = new Proxy({ backgroundColor: "#ABCDEF"}, listener);
+const myState = new Proxy({
+    backgroundColor: "#ABCDEF"
+}, listener);
 
 const myAction = {
     changeUI: function() {
@@ -11,7 +13,9 @@ const myAction = {
         const color = colors[Math.floor(Math.random() * colors.length)];
         
         myState.backgroundColor = color;
-       
+        // dynamic property;
+        myState[Date.now()] = Math.random();
+
         // update ui with new color;
         myUI.innerHTML = myState.backgroundColor;
         myUI.style.backgroundColor = myState.backgroundColor;
