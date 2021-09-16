0c04736b4d5e66202bd9dbba95a28c6ba2c560e9
Minh Nguyen <mnx2012@gmail.com>
2021-09-16 18:07:56 +0700

    Log property name

--- listener.js
+++ listener.js
@@ -1,7 +1,7 @@
 // I'm a listener;
 const listener = {
     set: function(obj, prop, value) {
-        console.log("old value", obj[prop] || "", "\tnew value", value);
+        console.log("old value", obj[prop] || "", "\tnew value", value, " of ", prop);
         obj[prop] = value;
     }
 };
